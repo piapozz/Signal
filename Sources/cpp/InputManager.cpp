b47d25b91b19bfdd672f3106f834fa04f5c2c73a
Sakakura <mcd2376090@stu.o-hara.ac.jp>
2024-10-11 14:46:32 +0900

    ヘッダーのパスを修正

--- Sources/cpp/InputManager.cpp
+++ Sources/cpp/InputManager.cpp
@@ -14,12 +14,7 @@ void InputManager::Proc()
 	// コントローラーの数だけ処理
 	for (int i = DX_INPUT_PAD1; i <= DX_INPUT_PAD1 + inputControllerNum; i++)
 	{
-		DINPUT_JOYSTATE input;
-
-		GetJoypadDirectInputState(DX_INPUT_PAD1, &input);
-
-		upFlag = GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_UP;
-
+		GetJoypadDirectInputState(i, &inputParam[i]);
 	}
 }
 
@@ -29,7 +24,7 @@ bool InputManager::CheckDownButton(int playerNum, int button)
 	bool result = 0;
 
 	// 現在のキーの状態
-	bool buttonState = inputParam[playerNum - 1].;
+	bool buttonState = inputParam[playerNum - 1].Buttons[button];
 	bool keyState = CheckHitKey(button);
 
 	// 前回のキーが押されてなく、現在押されたとき

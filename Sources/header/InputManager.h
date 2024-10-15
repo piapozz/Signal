8d3f2367954159da0f8fce70832684898564760b
Sakakura <mcd2376090@stu.o-hara.ac.jp>
2024-10-15 10:00:20 +0900

    二次元ベクトルの構造体を追加

--- Sources/header/InputManager.h
+++ Sources/header/InputManager.h
@@ -1,70 +1,23 @@
 #pragma once
+#include "Const.h"
 
 class InputManager
 {
-	enum INPUT
-	{
-		DOWN = 0,
-		LEFT,
-		RIGHT,
-		UP,
-		X,
-		Y,
-		A,
-		B,
-		LEFT_BUTTON,
-		RIGHT_BUTTON,
-		LEFT_TRIGGER,
-		RIGHT_TRIGGER,
-		LEFT_STICK_BUTTON,
-		RIGHT_STICK_BUTTON,
-
-		MAX
-	};
-
 private:
-	/*
-	struct InputParam
-	{
-		// 十字ボタンを押しているかのフラグ
-		bool upFlag = false;
-		bool leftFlag = false;
-		bool downFlag = false;
-		bool rightFlag = false;
 
-		// ABXYボタンを押しているかのフラグ
-		bool xFlag = false;
-		bool yFlag = false;
-		bool bFlag = false;
-		bool aFlag = false;
+	DINPUT_JOYSTATE inputParam[PLAYER_MAX];		// 入力情報の構造体
 
-		// LRボタン・トリガーを押しているかのフラグ
-		bool lbFlag = false;
-		bool rbFlag = false;
-		bool ltFlag = false;
-		bool rtFlag = false;
-
-		// LRスティックボタンを押しているかのフラグ
-		bool lsbFlag = false;
-		bool rsbFlag = false;
-
-		// スティックの入力情報
-		int lsInputX, lsInputY;
-		int rsInputX, rsInputY;
-	};
-	*/
-
-	DINPUT_JOYSTATE inputParam[2];		// 入力情報の構造体
+	bool downBuffer[PLAYER_MAX][32];		// キーの入力情報を保存するバッファ
 
 	int inputControllerNum;		// コントローラーの接続数
 
 public:
+
 	InputManager() {}
 	~InputManager() {}
 
 	void Proc();
-
-	void AddController();
-
-	bool CheckDownButton(int playerNum, int button);
+	bool GetButton(int playerNum, BUTTON button);
+	Vector2 GetStick(int playerNum, STICK stick);
+	bool CheckDownButton(int playerNum, BUTTON button);
 };

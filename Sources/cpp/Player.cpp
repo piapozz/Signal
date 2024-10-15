2c2ad63cb8947482349ce16f5f8d3c16bfef21ad
Sein <mcd2376075@stu.o-hara.ac.jp>
2024-10-15 10:38:59 +0900

    Player関係を変更 Vector2に対応させました。

--- Sources/cpp/Player.cpp
+++ Sources/cpp/Player.cpp
@@ -10,3 +10,35 @@ Player::~Player()
 
 }
 
+void Player::Proc()
+{
+	/*
+	if (canMove != false)
+	{
+		
+		if (inputManager->CheckStickDown())
+		{
+			// 移動方向と自身の向きを使って移動
+			Move(status.m_position, status.m_angle);
+
+			// 移動中に回避ボタンが押されたら
+			if (inputManager->CheckDownButton(playerNum, LEFT_TRIGGER) == true)
+			{
+				// 回避処理
+				Dodge(status.m_position, status.m_angle);
+			}
+		}
+			
+
+		// コントローラーの入力を受け取って自身を回転
+		// Rotate()
+
+		
+	}
+	*/
+}
+
+void Player::SetPlayerNum(int playerNumber)
+{
+
+}

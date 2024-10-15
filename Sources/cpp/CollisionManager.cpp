5a6ec13e9f9d36f4b34996a006b5b7ca9f54f763
Sakakura <mcd2376090@stu.o-hara.ac.jp>
2024-10-15 13:52:30 +0900

    InputManager一旦完成 Constのenumをenum classに変更

--- Sources/cpp/CollisionManager.cpp
+++ Sources/cpp/CollisionManager.cpp
@@ -1,1 +1,4 @@
 #include "../header/CollisionManager.h"
+
+// 法線
+// オブジェクト同士の判定とBulletBaseとObjectBaseとの判定
--- Sources/cpp/InputManager.cpp
+++ Sources/cpp/InputManager.cpp
@@ -1,40 +1,51 @@
 #include "../header/InputManager.h"
-#include "DxLib.h"
 
 void InputManager::Proc()
 {
 	// 接続されてるコントローラーの数を調べる
-	inputControllerNum = GetJoypadNum();
+	_inputControllerNum = GetJoypadNum();
+	printfDx("接続コントローラー%d\n", _inputControllerNum);
 
 	// コントローラーの数だけ処理
-	for (int i = DX_INPUT_PAD1; i <= DX_INPUT_PAD1 + inputControllerNum; i++)
+	for (int i = 0; i < _inputControllerNum; i++)
 	{
 		// コントローラーの入力情報を構造体に代入
-		GetJoypadDirectInputState(i, &inputParam[i]);
+		int result = GetJoypadDirectInputState(i + DX_INPUT_PAD1, &_inputParam[i]);
 	}
 }
 
-// 押されているか返す関数
-bool InputManager::GetButton(int playerNum, BUTTON button)
+// 接続コントローラーの数を返す関数
+int InputManager::GetPlayerNum()
 {
-	return inputParam[playerNum - 1].Buttons[button];
+	return _inputControllerNum;
+}
+
+// 押されているか返す関数
+bool InputManager::GetButton(int playerNum, Button button)
+{
+	bool result = false;
+
+	if (_inputParam[playerNum - 1].Buttons[(int)button] != 0)
+		result = true;
+
+	return result;
 }
 
 // スティックの入力情報を取得する関数
-Vector2 InputManager::GetStick(int playerNum, STICK stick)
+Vector2 InputManager::GetStick(int playerNum, Stick stick)
 {
 	Vector2 temp;
 
 	switch (stick)
 	{
-	case LEFT:
-		temp.x = inputParam[playerNum - 1].X;
-		temp.y = inputParam[playerNum - 1].Y;
+	case Stick::LEFT:
+		temp.x = (float)_inputParam[playerNum - 1].X;
+		temp.y = (float)_inputParam[playerNum - 1].Y;
 		break;
 
-	case RIGHT:
-		temp.x = inputParam[playerNum - 1].X;
-		temp.y = inputParam[playerNum - 1].Y;
+	case Stick::RIGHT:
+		temp.x = (float)_inputParam[playerNum - 1].Z;
+		temp.y = (float)_inputParam[playerNum - 1].Rz;
 		break;
 	default: break;
 	}
@@ -43,20 +54,22 @@ Vector2 InputManager::GetStick(int playerNum, STICK stick)
 }
 
 // 押された瞬間だけtrueを返す関数
-bool InputManager::CheckDownButton(int playerNum, BUTTON button)
+bool InputManager::CheckDownButton(int playerNum, Button button)
 {
 	// 結果
 	bool result = false;
 
 	// 現在のキーの状態
-	bool buttonState = inputParam[playerNum - 1].Buttons[button];
+	bool buttonState = false;
+	if (_inputParam[playerNum - 1].Buttons[(int)button] != 0)
+		buttonState = true;
 
 	// 前回のキーが押されてなく、現在押されたとき
-	if ((downBuffer[playerNum][button] == false) && (buttonState == true))
+	if ((_downBuffer[playerNum][(int)button] == false) && (buttonState == true))
 		result = true;
 
 	// 現在のキー状態をバッファに格納
-	downBuffer[playerNum][button] = buttonState;
+	_downBuffer[playerNum][(int)button] = buttonState;
 
 	return result;
 }

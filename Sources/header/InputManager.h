6afc3ea376924d79256d4fff88d471e853754bac
Sakakura <mcd2376090@stu.o-hara.ac.jp>
2024-10-15 13:52:30 +0900

    InputManager一旦完成 Constのenumをenum classに変更

--- Sources/header/InputManager.h
+++ Sources/header/InputManager.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "Const.h"
+#include "DxLib.h"
 
 class InputManager
 {
 private:
 
-	DINPUT_JOYSTATE inputParam[PLAYER_MAX];		// 入力情報の構造体
+	DINPUT_JOYSTATE _inputParam[PLAYER_MAX];		// 入力情報の構造体
 
-	bool downBuffer[PLAYER_MAX][32];		// キーの入力情報を保存するバッファ
+	bool _downBuffer[PLAYER_MAX][32];		// キーの入力情報を保存するバッファ
 
-	int inputControllerNum;		// コントローラーの接続数
+	int _inputControllerNum;		// コントローラーの接続数
 
 public:
 
@@ -17,7 +18,8 @@ public:
 	~InputManager() {}
 
 	void Proc();
-	bool GetButton(int playerNum, BUTTON button);
-	Vector2 GetStick(int playerNum, STICK stick);
-	bool CheckDownButton(int playerNum, BUTTON button);
+	int GetPlayerNum();
+	bool GetButton(int playerNum, Button button);
+	Vector2 GetStick(int playerNum, Stick stick);
+	bool CheckDownButton(int playerNum, Button button);
 };

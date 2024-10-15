2efbd5c58ece3494e97b57bfe7bd88518ef24a87
Sein <mcd2376075@stu.o-hara.ac.jp>
2024-10-15 12:07:30 +0900

    Player周りを変更 ・移動方法を変更 ・当たり判定管理関係の物を加えた

--- Sources/header/BaseObject.h
+++ Sources/header/BaseObject.h
@@ -4,15 +4,16 @@
 
 class BaseObject
 {
-protected:
+public:
 	struct Status
 	{
 		Vector2 m_position;		// 位置
 		float m_life;			// オブジェクトの体力
-		float m_shapeSize;		// 形の大きさ
 		float m_angle;			// 向き
+		float m_shapeSize;		// 形の大きさ
 	};
 
+protected:
 	Status status;
 
 	enum Shape				// 当たり判定の形区別するための情報
@@ -24,14 +25,15 @@ protected:
 	int objectModel;		// オブジェクトのモデル管理
 
 	bool canMove;			// 動けるかどうかを管理
-
 	bool canHit;			// 壁や弾などダメージが必要あるものかを区別
+	bool hitObject;			// オブジェクトにヒットしたかを管理
+	bool hitBullet;			// 弾にヒットしたかを管理
 
 	BaseObject();
 	~BaseObject();
 
 	void Draw();
 
-	void SetPosition(VECTOR objectPos);
+	void SetPosition(Vector2 objectPos);
 	void SetModelData(int model);
 };

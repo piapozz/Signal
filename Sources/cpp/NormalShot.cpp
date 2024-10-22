f7bbcf29bc20a36fe29685eb396cadb2f6a5ad47
Ishihara <mcd2376050@stu.o-hara.ac.jp>
2024-10-22 17:34:55 +0900

    融合弾実装中

--- Sources/cpp/NormalShot.cpp
+++ Sources/cpp/NormalShot.cpp
@@ -3,6 +3,8 @@
 
 void NormalShot::Move()  
 {
+	oldPosition = status.m_position;
+
 	status.m_position.x = status.m_position.x + cosf(status.m_angle) * _status.m_Speed;
 	status.m_position.y = status.m_position.y - sinf(status.m_angle) * _status.m_Speed;
 }

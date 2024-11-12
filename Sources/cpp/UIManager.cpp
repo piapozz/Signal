13f8f7c778af52129f5c4589a9b31a3aaa277e0a
Sein <mcd2376075@stu.o-hara.ac.jp>
2024-11-12 12:19:37 +0900

    レベルアップの処理をGameManagerからUIManagerへ移動した ・上記の処理を移動させた ・Initを追加した

--- Sources/cpp/UIManager.cpp
+++ Sources/cpp/UIManager.cpp
@@ -1,6 +1,6 @@
 #include "../header/UIManager.h"
 
-UIManager::UIManager()
+UIManager::UIManager(Vector2 initPos)
 {
 	bulletIcon[(int)BulletType::NORMAL] = LoadGraph("");
 	bulletIcon[(int)BulletType::EXPLOSION] = LoadGraph("");
@@ -13,6 +13,8 @@ UIManager::UIManager()
 	bulletStateText[(int)BulletStatus::POWER] = "POWER";
 	bulletStateText[(int)BulletStatus::RANGE] = "RANGE";
 	bulletStateText[(int)BulletStatus::RATE] = "RATE";
+
+	infoPos = initPos;
 }
 
 UIManager::~UIManager()
@@ -20,9 +22,9 @@ UIManager::~UIManager()
 
 }
 
-void UIManager::Init()
+void UIManager::Init(BulletManager* bullet)
 {
-
+	bulletManager = bullet;
 }
 
 void UIManager::Proc()
@@ -30,72 +32,74 @@ void UIManager::Proc()
 
 }
 
-void UIManager::Draw(int deviceNum, BulletManager* bullet)
+void UIManager::Draw(int deviceNum)
 {
-	// デバイス番号に応じてセッティングする
-	switch (deviceNum)
-	{
-	case 1:
-		infoHeight = PLAYER_HEIGHT_1;
-		infoWidth = PLAYER_WIDTH_1;
-
-		break;
-	case 2:
-
-
-		break;
-	case 3:
-
-
-		break;
-	case 4:
-
-
-		break;
-
-	}
-
-
 	// 取得している射撃タイプを画像で順番に並べる
-
-
 
 
 	// 取得している射撃タイプの下にレベルを表示する
 
-	// 射撃のステータスを星で描画する
-	// ステータスを０から順番に見る
-	//for (int i = 0; i < (int)BulletStatus::MAX; i++)
-	//{
-	//	DrawFormatString(infoHeight + i * 50, infoWidth, COLOR_BLACK, "Speed %f");
 
-	//	// ステータスの値によって星を描画
-	//	for (int j = 0; j < 5; j++)
-	//	{
-	//		if (i < bullet->GetBulletList()[deviceNum].m_BulletStatus[i]) { DrawFormatString(infoHeight + i * 50, infoWidth + j * 30, COLOR_BLACK, "★"); }
-	//		else DrawFormatString(infoHeight + i * 50, infoWidth + j * 30, COLOR_BLACK, "☆");
-	//	}
-	//}
-
+	// ステータスの状態を星で描画
 	for (int i = 0; i < 5; i++)
 	{
-		infoHeight += 100;
+		infoPos.y += 100;
 
 
-		DrawFormatString(infoHeight + i, infoWidth, COLOR_BLACK, bulletStateText[i].c_str());
+		DrawFormatString(infoPos.x, infoPos.y, COLOR_BLACK, bulletStateText[i].c_str());
 
 		// ステータスの値によって星を描画
 		for (int j = 0; j < (int)BulletStatus::MAX; j++)
 		{
-			if (i < bullet->GetBulletList()[deviceNum].m_BulletStatus[i]) { DrawFormatString(infoHeight + i * 30, infoWidth + j * 20, COLOR_BLACK, "★"); }
-			else DrawFormatString(infoHeight + i * 30, infoWidth + j * 20, COLOR_BLACK, "☆");
+			if (j < bullet->GetBulletList()[deviceNum].m_BulletStatus[i]) { DrawFormatString(infoPos.x + i * 30, infoPos.y, COLOR_BLACK, "★"); }
+			else DrawFormatString(infoPos.x + i * 30, infoPos.y + j * 20, COLOR_BLACK, "☆");
 		}
 	}
 }
 
 void UIManager::LevelUpUI()
 {
+	// レベルアップに必要な個数を満たしていたら
+	if (players[i]->GetExpValue() >= players[i]->request)
+	{
+
+		// レベルアップしたら次のレベルアップに必要なコストを上げる
+		players[i]->request += 3;
+
+		// 三の倍数回これまでにレベルアップしていたらパワーアップから選ぶ
+		if (players[i]->levelUpCount % 3)
+		{
+			// パワーアップできる回数を一あげる
 
 
+		}
+
+		// 三の倍数回ではなかったらステータスから選ぶ
+		else
+		{
+			// ステータスアップできる回数を一あげる
+
+
+		}
+
+	}
+
+	// レベルアップできる回数を見てコントローラーの入力を受け取り結果を反映させる
+	// パワーアップを行う
+	if (players[i]->powerUpCount)
+	{
+		// 列挙体BulletTypeの大きさを使って乱数を取得 
+		GetRand((int)BulletType::MAX);
+
+
+	}
+
+	// ステータスアップを行う
+	else if (players[i]->statusUpCount)
+	{
+
+		GetRand((int)BulletStatus::MAX);
+
+	}
 
 }
